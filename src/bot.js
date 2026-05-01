3c60cfbb411be4df4d6b4fb40b41ab5340f15a1e
Buruza-gg <isawan.lglt@icloud.com>
2026-05-01 15:31:18 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
+if (!process.env.BOT_TOKEN) {
+  throw new Error('BOT_TOKEN не найден в .env файле!');
+}
+
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
 module.exports = bot;
